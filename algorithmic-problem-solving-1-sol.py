1538393615371755dc39ee74e0774fe8eb09f0e3
physicalrobot <kimboho@gmail.com>
2023-11-16 12:44:13 -0800

    first problem, added test cases

--- algorithmic-problem-solving-1-sol.py
+++ algorithmic-problem-solving-1-sol.py
@@ -1,2 +1,14 @@
 # Look through the given array for an element that doesn't repeat.
 
+# Test Cases
+
+# Input: nums = [2,2,1]
+# Output: 1
+# Example 2:
+
+# Input: nums = [4,1,2,1,2]
+# Output: 4
+# Example 3:
+
+# Input: nums = [1]
+# Output: 1
