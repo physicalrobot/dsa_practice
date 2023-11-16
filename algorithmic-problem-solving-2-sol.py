850e5eb77617cea684a179d8040e6f4fed643637
physicalrobot <kimboho@gmail.com>
2023-11-16 12:55:48 -0800

    test case for problem 2

--- algorithmic-problem-solving-2-sol.py
+++ algorithmic-problem-solving-2-sol.py
@@ -1,2 +1,10 @@
 # We need to find the sum of all mulitples of 3 and 5 below 1000. 
 # We can start by iterating to 1000 and saving all the values that are divisible by 3 and 5 in a value and adding to the value when hit another divisible number.
+
+# Test Case
+
+# Example 1
+
+# n = 10
+# 3,5,6,9
+# sum = 23
