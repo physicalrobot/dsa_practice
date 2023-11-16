78cc783f3509a9ae1f8e7b7bc52fa976f7321acb
physicalrobot <kimboho@gmail.com>
2023-11-16 12:48:50 -0800

    problem 1 solution code

--- algorithmic-problem-solving-1-sol.py
+++ algorithmic-problem-solving-1-sol.py
@@ -1,14 +1,32 @@
 # Look through the given array for an element that doesn't repeat.
 
+def singleNumber(nums):
+    # Initialize a variable to store the single element
+    single_element = 0
+
+    # XOR all the elements in the array
+    for num in nums:
+        single_element ^= num
+
+    return single_element
+
+
 # Test Cases
 
+# Example 1:
 # Input: nums = [2,2,1]
 # Output: 1
-# Example 2:
 
+print(singleNumber([2,2,1]))
+
+# Example 2:
 # Input: nums = [4,1,2,1,2]
 # Output: 4
-# Example 3:
 
+print(singleNumber([4,1,2,1,2]))
+
+# Example 3:
 # Input: nums = [1]
 # Output: 1
+
+print(singleNumber([1]))
