0e44fc9f7b04787a679bd89cc195dbea195de7b5
physicalrobot <kimboho@gmail.com>
2023-11-30 13:13:09 -0700

    solution to counter-problem

--- counter-problem.py
+++ counter-problem.py
@@ -2,3 +2,20 @@
 
 # Check to see if each letter in ransomNote can be constructed from magazine.
 
+def canConstruct(self, ransomNote, magazine):
+    
+        magazine_letters = {}
+        
+        for letter in magazine:
+            if letter in magazine_letters:
+              magazine_letters[letter] += 1
+            else:
+                magazine_letters[letter] = 1
+        
+        for letter in ransomNote:
+            if letter in magazine_letters and magazine_letters[letter] > 0:
+                magazine_letters[letter] -= 1
+            else:
+                return False
+            
+        return True
