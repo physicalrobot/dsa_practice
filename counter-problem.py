a59f5eaa922d032a661a9cbec1e01cc7f004d701
physicalrobot <kimboho@gmail.com>
2023-11-30 13:12:43 -0700

    added pseudocode

--- counter-problem.py
+++ counter-problem.py
@@ -1,3 +1,4 @@
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-     
+# Create a dictionary to store the frequency of each letter in magazine.
+
+# Check to see if each letter in ransomNote can be constructed from magazine.
+
